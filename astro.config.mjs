bf8b512c7a4c669f4e78089dd236ca925e3ca1b2
SamV1ce <hyttinen.sami@gmail.com>
2026-06-12 10:23:52 +0300

    Fix astro.config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://SamV1ce.github.io',           // ← Change if your username is different
+  base: '/samvice',                     // ← Change this to your exact repo name
+  integrations: [tailwind()],
 });
